e63a91be4048cf89a2d95ed51490dff9a56ecd08
lautaromoreldv <lautaro.morel@davinci.edu.ar>
2021-11-24 20:40:17 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let buscar = d.getElementById('buscar');
 let input = d.getElementById('input');
 let info = d.getElementById('info');
 //let mostrar = d.getElementById('mostrar');
-let alert = d.getElementById('alert');
+let alerta = d.getElementById('alerta');
 let vermastarde = d.getElementById('vermastarde');
 let liVermastarde = d.getElementById('liVermastarde');
 let home = d.getElementById('home');
@@ -44,48 +44,60 @@ function obtenerDato(valor){
             const plot = data.Plot;
             const poster = data.Poster;
             const score = data.imdbRating;
-            info.innerHTML = `<div class="card mt-3 mb-3">
-                                <img class="card-img-top" src="${poster}" alt="${title}" />
-                                <div class="card-body">
-                                    <h2 class="text-center">${title}</h2>
-                                </div>  
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">
-                                        <p>Descripción:</p>
-                                        <span>${plot}<span>
-                                    <li class="list-group-item">
-                                        <p>Calificación:</p>
-                                        <span><strong>${score}/10</strong><span>
-                                </ul>  
-                                <div class="text-center mt-3 mb-3">
-                                    <button type="button" id="porver" class="btn btn-outline-info text-center">Ver más tarde</button>
-                                </div>
-                            </div>`;
-                                let porver = d.getElementById('porver');
-                                porver.addEventListener('click', (e) => {
-                                    e.preventDefault();
-                                    porver.parentNode.remove();
-                                    db.peliculas.put({ 
-                                        titulo: title, 
-                                        poster: poster,
-                                        sinopsis: plot, 
-                                        score: score,
-                                        _id: String(Date.now())
-                                    })
-                                    window.scrollTo(0,0);
+            alerta.innerHTML = `<div class="d-flex justify-content-center">
+                                    <div class="spinner-grow text-warning" role="status">
+                                        <span class="sr-only">Loading...</span>
+                                    </div>
+                                </div>`;
+            setTimeout(() => {
+                alerta.innerHTML = '';
+            }, 1500);
 
-                                    alert.innerHTML = `<div class="alert alert-success" role="alert">
-                                                        Se agregó con éxito a la sección Ver más tarde
-                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                                            <span aria-hidden="true">&times;</span>
-                                                        </button>
-                                                        </div>`;
-                                    setTimeout(() => {
-                                        alert.innerHTML = '';
-                                    }, 3000);
-                                    
-                                });
-                                input.value = '';                        
+            setTimeout(() => {
+                info.innerHTML = `<div class="card mt-3 mb-3 role="dialog">
+                                    <img class="card-img-top" src="${poster}" alt="${title}" />
+                                    <div class="card-body">
+                                        <h2 class="text-center">${title}</h2>
+                                    </div>  
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">
+                                            <p>Descripción:</p>
+                                            <span>${plot}<span>
+                                        <li class="list-group-item">
+                                            <p>Calificación:</p>
+                                            <span><strong>${score}/10</strong><span>
+                                    </ul>  
+                                    <div class="text-center mt-3 mb-3">
+                                        <button type="button" id="porver" class="btn btn-outline-info text-center">Ver más tarde</button>
+                                    </div>
+                                </div>`;
+                let porver = d.getElementById('porver');
+                porver.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    porver.parentNode.remove();
+                    db.peliculas.put({ 
+                        titulo: title, 
+                        poster: poster,
+                        sinopsis: plot, 
+                        score: score,
+                        _id: String(Date.now())
+                    })
+                    window.scrollTo(0,0);
+
+                    alerta.innerHTML = `<div class="alert alert-success" role="alert">
+                                        Se agregó con éxito a la sección Ver más tarde
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        </div>`;
+                    setTimeout(() => {
+                        alerta.innerHTML = '';
+                    }, 3000);
+                    
+                });
+                input.value = '';            
+            }, 1500);
+                        
         })   
 }        
 
@@ -105,7 +117,6 @@ function refreshView(){
 
 function mostrarPeliculas(peliculas){
     for (let i = 0; i < peliculas.length; i++) {
-        console.log(peliculas[i]);
         vermastarde.innerHTML = `<div class="card">
                                     <img class="card-img-top" src="${peliculas[i].poster}" alt="${peliculas[i].titulo}" />
                                     <div class="card-body">
@@ -132,9 +143,6 @@ function mostrarPeliculas(peliculas){
                                     porverdelete.parentNode.remove();
                                 })
     }
-    /*peliculas.forEach(function(pelicula, peli) {
-        
-    });*/
 }
 
 
@@ -144,24 +152,39 @@ window.onload = function (){
 }
 
 window.addEventListener("offline", (e) => {
-    console.log('offine');
-    let nav = d.getElementsByClassName('.navbar');
-    nav.classList.add('off');
-    console.log(nav);
+    console.log('offline');
+    alerta.innerHTML = `<div class="alert alert-dark" role="alert">
+                            ¡No hay conexión a internet!
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>`;
+    let nav = d.getElementById('navbar');
+    nav.classList.remove('online');
+    nav.classList.add('offline');
   });
   
-window.addEventListener("online", (e) => {
+window.addEventListener("online", () => {
     console.log('online');
-    let nav = d.getElementsByClassName('.navbar');
-    nav.classList.add('on');
+    alerta.innerHTML = '';
+    let nav = d.getElementById('navbar');
+    nav.classList.remove('offline');
+    nav.classList.add('online');
   });
   
   // Escucho si el navegador esta online o no, util en los casos que entro sin conexion.
   if (!navigator.onLine) {
     console.log("estoy sin conexion!!");
-    let nav = d.getElementsByClassName('.navbar');
-    nav.classList.remove('on', 'bg-light');
-    nav.classList.add('off');
+    alerta.innerHTML = `<div class="alert alert-dark" role="alert">
+                            Entraste a esta página pero no tenes internet :c
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>`;
+    let nav = d.getElementById('navbar');
+    nav.classList.remove('online');
+    nav.classList.add('offline');
+
   }
 
   $(function(){ 
@@ -172,3 +195,28 @@ window.addEventListener("online", (e) => {
     });
 });
 
+let installButton = document.createElement('button');
+let prompt;
+
+window.addEventListener('beforeinstallprompt', function(e){
+  e.preventDefault();
+  prompt = e;
+});
+
+installButton.addEventListener('click', function(){
+   prompt.prompt();
+})
+
+let installed = false;
+installButton.addEventListener('click', async function(){
+  prompt.prompt();
+  let result = await that.prompt.userChoice;
+  if (result&&result.outcome === 'accepted') {
+     installed = true;
+  }
+})
+
+window.addEventListener('appinstalled', async function(e) {
+    installButton.style.display = "none";
+ });
+ 
